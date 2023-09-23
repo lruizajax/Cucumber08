7cfcaae3362e350c15a9173b2fea9c65e7bfb3b9
Luis Ruiz <lruizajax@gmail.com>
2023-09-23 07:38:54 -0500

    removing duplicate plugins

--- src/test/java/com/magento/steps/RegisterSteps.java
+++ src/test/java/com/magento/steps/RegisterSteps.java
@@ -12,14 +12,16 @@ import java.util.Locale;
 public class RegisterSteps extends Base {
 
     Faker faker = new Faker(new Locale("en-US"));
+
     @Given("user is on register page {string}")
     public void user_is_on_register_page(String url) {
-        driver.navigate().to(BASE_URL+url);
+        driver.navigate().to(BASE_URL + url);
     }
+
     @When("user fill out form with his information")
     public void user_fill_out_form_with_his_information() {
         String email = faker.internet().emailAddress();
-        String password = faker.internet().password(8,10,true,true);
+        String password = faker.internet().password(8, 10, true, true);
         driver.findElement(By.id("firstname")).sendKeys(faker.address().firstName());
         driver.findElement(By.id("lastname")).sendKeys(faker.address().lastName());
         driver.findElement(By.id("email_address")).sendKeys(email);
@@ -36,6 +38,7 @@ public class RegisterSteps extends Base {
     public void user_clicks_on_register_button() {
         driver.findElement(By.xpath("//*[@id='form-validate']/div/div[1]/button/span")).submit();
     }
+
     @Then("message diplayed {string}")
     public void message_diplayed(String msg) {
 
